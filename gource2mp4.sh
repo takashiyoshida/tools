28c9fa8db4459dc3c5083c63f1b003a5cb257d43
Takashi Yoshida <takashi@takashiyoshida.org>
2010-05-24 21:28:24 +0800

    Added comments about TFSConverter and x264 encoding.

--- gource2mp4.sh
+++ gource2mp4.sh
@@ -4,6 +4,12 @@
 # This script uses Gource to generate a PPM video stream from source control 
 # log (Git, Mercurial, etc) and save the video stream to MPEG4 format.
 #
+# This script is intended to take output from TFSConverter by mikeashley.
+# TFSConverter | <http://github.com/mikeashley/TFSConverter>
+#
+# I actually have not tested the script with Git or Mercurial based 
+# projects.
+#
 
 GOURCE=/opt/local/bin/gource
 GOURCE_OPT="--disable-progress --stop-at-end --output-ppm-stream -"
@@ -12,6 +18,8 @@ FFMPEG=/opt/local/bin/ffmpeg
 FFMPEG_OPT="-y -b 3000K -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -vpre"
 
 # See other available options in /opt/local/share/ffmpeg
+# Also see Robert Swan's weblog about FFMPEG and x264 encoding:
+# <http://rob.opendot.cl/index.php/useful-stuff/ffmpeg-x264-encoding-guide/>
 FFMPEG_VPRE="libx264-default"
 
 if [ $# != 2 ]; then
